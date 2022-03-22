3323694aaf4148297e2c1184ee2585f00a209e56
Jin1won <wlsdnjs16521@gmail.com>
2022-03-23 00:31:51 +0900

    Navigation Props

--- App.js
+++ App.js
@@ -6,42 +6,18 @@ import { Ionicons } from "@expo/vector-icons";
 import { Asset, useAssets, useFonts } from "expo-asset";
 import { NavigationContainer } from "@react-navigation/native";
 import Tabs from "./navigation/Tabs";
-//if we use useAssets and useFonts hooks, those codes are not needed anymore.
-// const loadFonts = (fonts) => fonts.map((font) => Font.loadAsync(font));
-// const loadImages = (images) =>
-//   images.map((image) => {
-//     if (typeof asset === "string") {
-//       return Image.prefetch(image);
-//     } else {
-//       return Asset.loadAsync(image);
-//     }
-//   });
+import Stack from "./navigation/Stack";
 
 export default function App() {
   const [assets] = useAssets([require("./picture.png")]);
   const [loaded] = Font.useFonts(Ionicons.font);
-  // const [ready, setReady] = useState(false);
-  // const onFinish = () => setReady(true);
-  // const startLoading = async () => {
-  //   const fonts = loadFonts([Ionicons.font]);
-  //   const images = loadImages([
-  //     require("./picture.png"),
-  //     "https://d33wubrfki0l68.cloudfront.net/b152eb4214943f96e83c4babde026b12221e68f1/a20c2/img/oss_logo.png",
-  //   ]);
-  //   await Promise.all([...fonts, ...images]);
-  // };
+
   if (!assets || !loaded) {
-    return (
-      <AppLoading
-      // startAsync={startLoading}
-      // onFinish={onFinish}
-      // onError={console.error}
-      />
-    );
+    return <AppLoading />;
   }
   return (
     <NavigationContainer>
-      <Tabs />
+      <Stack />
     </NavigationContainer>
   );
 }
